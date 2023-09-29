41e7a7d5657f3c51269a49eba3c0b6dfbb354db5
Dorem1x <dorem1x@yandex.ru>
2023-09-29 11:59:14 +0300

    project done

--- src/main/java/com/tictactoe/Field.java
+++ src/main/java/com/tictactoe/Field.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 public class Field {
     private final Map<Integer, Sign> field;
 
+
     public Field() {
         field = new HashMap<>();
         field.put(0, Sign.EMPTY);
